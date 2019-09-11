172e681a5457e300d464f0c22261ed5abaa576ec
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-09-11 10:09:48 +0700

    refactoring method etract

--- src/Cylinder.php
+++ src/Cylinder.php
@@ -10,9 +10,16 @@ class Cylinder
 {
     public function getVolume($radius, $height)
     {
-        $baseArea = pi() * $radius * $radius;
-        $perimeter = 2 * pi() * $radius;
-        $volume = $perimeter * $height + 2 * $baseArea;
+
+        $volume = $this->getPerimeter($radius) * $height + 2 * $this->getBaseArea($radius);
         return $volume;
+    }
+    public function getPerimeter($radius){
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+    public function getBaseArea($radius){
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
     }
 }
